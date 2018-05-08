f05e57234536ee4d2dc80a9e65e35a2169ebf306
DouglasCastro21 <douglascastro21>
2018-05-08 18:40:00 -0300

    add tela cadastro

--- app/src/main/java/bike/douglas/com/bikejanu/Activity/Tab1Entrar.java
+++ app/src/main/java/bike/douglas/com/bikejanu/Activity/Tab1Entrar.java
@@ -3,12 +3,14 @@ package bike.douglas.com.bikejanu.Activity;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -24,13 +26,25 @@ public class Tab1Entrar extends Fragment {
     private FirebaseAuth autenticacao;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.tab1entrar, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View Viewroot = inflater.inflate(R.layout.tab1entrar, container, false);
 
-        return rootView;
 
-       // edtEmail = findViewById(R.id.EmailID);
+
+        TextView edtEmail = (TextView) Viewroot.findViewById(R.id.EmailID);
+        TextView edtSenha = (TextView) Viewroot.findViewById(R.id.SenhaID);
+        Button btnLogar = (Button) Viewroot.findViewById(R.id.LogarID);
+
+        btnLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
+        return Viewroot;
 
     }
 
